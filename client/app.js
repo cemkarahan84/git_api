8843610e09af2581be6d7adefcac2a5faa37373f
cemkarahan84 <cem.karahan84@gmail.com>
2017-09-29 00:02:02 +0200

    Add data and ui
    not finished yet but there is an exemple of fetched data

--- client/app.js
+++ client/app.js
@@ -2,37 +2,42 @@
  * Déclaration de l'application
  */
 
+'use strict'
 
 
 
 var myApp = angular.module('myApp', [
-    // Dépendances du "module"
-    'ngRoute',
-    'routeAppControllers',
-    'chart.js'
+  // Dépendances du "module"
+  'ngRoute',
+  'routeAppControllers',
+  'chart.js',
+  'ngAnimate'
+
 ]);
+
+
 
 var routeAppControllers = angular.module('routeAppControllers', []);
 
 
 
 
-
-myApp.config(['$routeProvider','$locationProvider',
-    function($routeProvider,$locationProvider) {
-
-
-        // Système de routage
-        $routeProvider
-        .when('/', {
-            templateUrl: 'home.html',
-            controller: 'homeCtrl'
-        }).otherwise({
-            redirectTo: '/'
-        });
+//configuration du routeur
+myApp.config(['$routeProvider', '$locationProvider',
+  function($routeProvider, $locationProvider) {
 
 
-    }
+    // Système de routage
+    $routeProvider
+      .when('/', {
+        templateUrl: 'home.html',
+        controller: 'homeCtrl'
+      }).otherwise({
+        redirectTo: '/'
+      });
+
+
+  }
 
 
 ]);
@@ -42,75 +47,96 @@ myApp.config(['$routeProvider','$locationProvider',
 
 
 
-var search = angular.module('search', []);
-
-
-routeAppControllers.controller('homeCtrl',['$scope','$http',function($scope,$http){
 
 
 
+routeAppControllers.controller('homeCtrl', ['$scope', '$http', function($scope, $http) {
+
+
+  //charts datas
   $scope.labels = [];
   $scope.data = [];
 
-$scope.details=null;
-
-  $scope.searchRepos=function()
-  {
-       var search = {query :$scope.search,
-                    };
+  $scope.details = null;
 
 
-       $http.get('/search',{params:search}).success(
-        function(search){
-            console.log(search)
-search =
+//function recherche
+  $scope.searchRepos = function() {
+    var search = {
+      query: $scope.search,
+    };
 
-            $scope.response=search;
+//route http pour la recherche envoyé au back
+    $http.get('/search', {
+      params: search
+    }).success(
+      function(search) {
+        console.log(search)
+        search =
+          $scope.response = search;
+      }).error(
+      function(error) {
+        console.log(error)
+      });
+  }
 
-        }).error(
-        function(error){
-            console.log(error)
-        });
-}
+//Fonction pour voir les details
+  $scope.viewDetails = function(fullname) {
 
-$scope.viewDetails=function(fullname)
-{
+//on formatte la requete
+    let query = {
+      query: fullname
+    };
 
-let query={
-  query: fullname
-};
+//route http details au back
+    $http.get('/details', {
+      params: query
+    }).success(
+      function(query) {
+        $scope.details = query;
+
+        console.log(query.contributors);
+
+        let listName = [];
+        let listData = [];
+
+        $scope.labels = listName;
+        $scope.data = listData;
+
+        //boucle de maniere a avoir un tableau uniquement des logins et des nombres de contributions
+        for (var key in query.contributors) {
+          listName.push(query.contributors[key].login);
+          listData.push(query.contributors[key].contributions);
+        }
+
+        console.log(listName);
+      }).error(
+      function(error) {
+        console.log(error)
+      });
+
+//route pour recuperer les commits
+    $http.get('/commits', {
+      params: query
+    }).
+    success(function(query) {
+
+      $scope.commits = query;
+
+    })
 
 
-  $http.get('/repos',{params:query}).success(
-   function(query){
-     $scope.details=query;
 
-console.log(query.contributors);
+  }
 
-let listName=[];
-let listData=[];
 
-$scope.labels=listName;
-$scope.data=listData;
 
-     for (var key in query.contributors) {
-       listName.push(query.contributors[key].login);
-       listData.push(query.contributors[key].contributions);
-     }
-
-console.log(listName);
-   }).error(
-   function(error){
-       console.log(error)
-   });
-
-}
 
 
 }]);
 
-
-routeAppControllers.controller('reposCtrl',['$scope','$http',function($scope,$http){
+//not used yet
+routeAppControllers.controller('reposCtrl', ['$scope', '$http', function($scope, $http) {
 
 
 
--- server.js
+++ server.js
@@ -1,25 +1,29 @@
-
-
 'use strict';
 
 //  Déclaration des Requires
 
-const express       = require('express');
-const app           = express();
+const express = require('express');
+const app = express();
 var request = require('request');
-const router =  express.Router();
+const router = express.Router();
 
-const http          = require('http').Server(app);
-const path          = require('path');
+const http = require('http').Server(app);
+const path = require('path');
+
+
 
 
 // Déclaration des variables
 let port = process.env.PORT || 3000;
 
+
+
+
+
 console.log(port);
 
 
-var resp= {};
+var resp = {};
 // Routing de l'application pour definir le front client
 app.use(express.static(path.join(__dirname, 'client')));
 
@@ -29,69 +33,109 @@ http.listen(port, () => {
 });
 
 //Route get qui ecoute le front pour la reception de queries
-app.get('/search', function (req, res) {
+app.get('/search', function(req, res) {
 
-  var query= req.query;
-  var queryOption =query.query;
+  var query = req.query;
+  var queryOption = query.query;
 
 
-  console.log(query.select);
+
 
   //Declaration des options pour interroger l'api github
   var options = {
-    url: 'https://api.github.com/search/repositories?q='+queryOption,
+    url: 'https://api.github.com/search/repositories?q=' + queryOption,
     headers: {
       'User-Agent': 'request'
     }
   };
+
+
+
+
   // l'appel de la requete
-  request(options, function (error, response, body) {
-    console.log('statusCode:', response && response.statusCode);
+  var test = [];
+
+  request(options, function(error, response, body) {
+
     var obj = JSON.parse(body);
+
 
     res.send(JSON.stringify(obj.items));
   });
 
+
+
+
+
+
+
+
+
 });
 
+//route listener pour recevoir la demande de commits ,la traiter , et renvoyer la data
+app.get('/commits', function(req, res) {
 
-app.get('/repos',function(req,res){
-
-  var query =req.query.query;
-  var options = {
-    url: 'https://api.github.com/repos/'+query ,
+  let query = req.query.query;
+  let options = {
+    url: 'https://api.github.com/repos/' + query + '/commits',
     headers: {
       'User-Agent': 'request'
     }
   };
-  console.log(options);
 
-  request(options, function (error, response, body) {
+//on fetch la data sur github
+  request(options, (error, response, body) => {
+
+    let tmp = JSON.parse(body);
+
+    res.send(tmp); //on renvoi au front
+
+  })
+
+})
+
+//route pour les details
+app.get('/details', function(req, res) {
+
+  var query = req.query.query;
+
+
+  var details = {
+    url: 'https://api.github.com/repos/' + query,
+    headers: {
+      'User-Agent': 'request'
+    }
+  };
+
+//fetch de data details
+  request(details, function(error, response, body) {
     var obj = JSON.parse(body);
 
 
-    var committersOptions={
-      url:'https://api.github.com/repos/'+query+'/contributors',
+    var committersOptions = {
+      url: 'https://api.github.com/repos/' + query + '/contributors',
       headers: {
         'User-Agent': 'request'
       }
     };
 
+//fetch de data des contributeurs
+    request(committersOptions, function(error, response, body) {
+      var obj2 = JSON.parse(body)
 
-    request(committersOptions,function(error,response,body)
-    {
-      var obj2= JSON.parse(body)
+      var send = {
+        repos: obj,
+        contributors: obj2
+      };
 
-      var send={repos:obj,
-        contributors: obj2};
-
-        res.send(JSON.stringify(send));
-      });
-
-
-
+      res.send(JSON.stringify(send)); //on envoi au front avec les deux fetch 
     });
 
-    console.log(res);
 
-  })
+
+  });
+
+  //  console.log(res);
+
+})
